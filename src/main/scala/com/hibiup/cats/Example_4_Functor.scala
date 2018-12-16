61e5dbdfc7b81f07841b16a759dfe8869e18ab7d
326487162 <Welcome@661>
2018-12-16 00:11:46 -0500

    Add Functor example

--- src/main/scala/com/hibiup/cats/Example_4_Functor.scala
+++ src/main/scala/com/hibiup/cats/Example_4_Functor.scala
@@ -12,10 +12,10 @@ package com.hibiup.cats
         override def imap[A, B](fa: F[A])(f: A => B)(g: B => A): F[B] = map(fa)(f)
         ...
 
-        * Functor　提供了 compose 和 composeContravariant 方法作为实例方法的绑定函数．之所以是 compose 而不是
-        * 我们熟悉的 apply 是因为根据 Functor 本身的定义，map 方法必须接受一个高阶类型，并且这个高阶类型允许进一步
-        * 展开为一连串的递归调用，也就是说 Functor 必须允许形式为: _.map(_.map(_.map(f)))...的运算,因此它的参数
-        * 类型必须是 F[G[_]]
+        * 除了 map 外，Functor　还提供了 compose 和 composeContravariant 方法来绑定更复杂的组合类型，一个组合类
+        * 意味着一个容器套着另外一个容器，例如 List[Either[String, Future[A]]]，那么如果展开的话，我们实际上在处理
+        * 一个 _.map(_.map(_.map(f)))... 的循环嵌套操作。为此 Functor 提供了 compose 方法来处理此类参数类型。它
+        * 允许参数的形式是 F[G[_]]
         *
         def compose[G[_]: Functor]: Functor[λ[α => F[G[α]]]] =
           new ComposedFunctor[F, G] {
@@ -30,15 +30,38 @@ package com.hibiup.cats
           }
       }
   *
-  * 接下来以下面例子进一步说明：
+  * 接下来以进一步说明：
   * */
-object Example_4_Functor {
+
+/**
+  * 一）首先以一个简单的map 为例：
+  * */
+object Example_4_Functor_1 {
     /** 1）不需要自己定义Functor了，可以直接引进 */
     import cats.Functor
 
     /**
-      * 2）定义要处理的数据。注意 compose 对参数类型的需求是 F[G[_]]，也就是说必须是高阶套高阶，不可以是 F[Int]。详细参考
-      * 上面的说明
+      * 2）定义要处理的数据。
+      * */
+    val listOption = List(1, 2, 3)
+
+    /** 3）必须 import 数据类型对应包中的隐式方法。*/
+    import cats.instances.list._
+
+    /** 4）绑定 map 函数的处理实体。*/
+    val functor = Functor[List].map(listOption) { _ + 1 }  // List(2, 3, 4)
+}
+
+/**
+  * 二）处理一个嵌套类型:　F[G[_]]
+  * */
+object Example_4_Functor_2 {
+    /** 1）引进 Functor typeclass */
+    import cats.Functor
+
+    /**
+      * 2）定义要处理的数据。注意 compose 对参数类型的需求是 F[G[_]]，也就是说必须是嵌套类型，不可以是 F[Int]。
+      * 详细参考上面的说明
       * */
     val listOption = List(Some(1), None, Some(2))
 
@@ -47,5 +70,51 @@ object Example_4_Functor {
     import cats.instances.option._
 
     /** 4）通过 compose 绑定 map 函数的处理实体。*/
-    val functor = Functor[List].compose[Option].map(listOption) { _ + 1 }
+    val functor = Functor[List].compose[Option].map(listOption) { _ + 1 }  // List(Some(2), None, Some(3))
+}
+
+/**
+  * 三）更复杂的情况是假设 Functor 本身也希望进行嵌套，也就是说 Functor 的类型参数将变成描述自己的 F[_]，而不是
+  * List 之类的具体的高阶类型．
+  * */
+object Example_4_Functor_3 {
+    import cats.Functor
+
+    /** 设计一个 Functor, 这个 Functor 是我们将要嵌套传递的. */
+    val listOption = List(Some(1), None, Some(2))
+
+    /** 1）定义一个函数，这个函数的作用是将一个包含数据的容器映射成包含函数的容器，F[A] => F[Unit]
+      * 所以它的参数是F[A] 返回值是 F[Unit]．*/
+    def NeedsFunctor[F[_]: Functor, A](fa: F[A]): F[Unit] =
+    /** 2）定义它的 map 函数体 */
+        Functor[F].map(fa)(_ => ())
+
+    /** 3) 将 List(Some(1), None, Some(2)) => List(Some(()), None, Some(())) */
+    import cats.instances.list._
+    import cats.instances.option._
+    val listOptionFunctor = Functor[List].compose[Option]
+
+    /**
+      * 3-1) 定义一个 type lambda: ({type λ[A] =  List[Option[A]]})#λ, 作用是将类型 Option 作为缺省，只关注类型 A
+      **/
+    type ListOption[A] = List[Option[A]]
+    /** 3-2) 这是两次连续函数调用，第一级调用是：NeedsFunctor[ListOption, Int](listOption)，传入
+      * List(Some(1), None, Some(2))，然后再结果上直接调用 map(listOptionFunctor) 得到List(Some(()), None, Some(())).
+      *
+      * 之所以要以这种方式连续调用，是因为我们要将 listOptionFunctor　作用于　Option[A] 中的 A，而不是整个 Option[A]
+      * 也就是说我们要实现　Option(fa(a))，否则就成了 fa(Option(a))．也因此我们才需要定义上面的这个 type lambda．
+      * 根据 type lambda 提取出 A，和它的类型 Int，也就是说在第一级函数调用 “NeedsFunctor[ListOption, Int](listOption)”
+      * 中，参数 ListOption == F[_] == ListOption[Int]，然后结果执行函数调用：map(listOptionFunctor) 时 listOptionFunctor
+      * 就作用于 Int，而不是 Option[Int] 了.
+      * */
+    val mapper = NeedsFunctor[ListOption, Int](listOption)(listOptionFunctor)
+
+    /** 4) 得到 mapper．将 List(Some(1), None, Some(2)) 重新作用于它，此时的 mapper 只需等待容器内的　() 被赋予
+      * 实际有效的函数体．*/
+    import cats.data.Nested
+    import cats.syntax.functor._
+    val nested: Nested[List, Option, Int] = Nested(listOption)
+
+    /** 5) 赋予函数体，完成计算 */
+    val r = nested.map(_ + 1)
 }
--- src/test/scala/com/hibiup/cats/Example_4_Functor.scala
+++ src/test/scala/com/hibiup/cats/Example_4_Functor.scala
@@ -3,8 +3,23 @@ package com.hibiup.cats
 import org.scalatest.FlatSpec
 
 class Example_4_Functor_test extends FlatSpec{
-    "Functor test" should "" in {
-        import com.hibiup.cats.Example_4_Functor._
+    "Functor map test" should "" in {
+        import com.hibiup.cats.Example_4_Functor_1._
         println(functor)
+    }
+
+    "Functor compose test" should "" in {
+        import com.hibiup.cats.Example_4_Functor_2._
+        println(functor)
+    }
+
+    "Functor in Functor test" should "" in {
+        /*def func1[A, B](a:A)(b:A=>B):(B,B) = (b,b)
+        def func2[C](c:C) = func1(c)(_ => _)
+        func2(1)(2)*/
+
+        import com.hibiup.cats.Example_4_Functor_3._
+        println(mapper)
+        println(r)
     }
 }
